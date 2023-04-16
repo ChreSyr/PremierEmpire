f10668b8e7fef94dfc3fc6ccd4e9e0e17d681100
ACER-SyméonRB <dev.chresyr@gmail.com>
2023-04-16 17:20:40 +0200

    Updated todos

--- library/todo.py
+++ library/todo.py
@@ -4,19 +4,19 @@
 # TODO : Tester le jeu sur un Chromebook (si possible)
 
 # BUGS :
-
-# DIVERS :
-# TODO : quand les règles seront terminées, faire un tutoriel
+# TODO : solve : un jour une couleur n'avait plus de soldat mais le compteur était à 2
+# TODO : solve : un jour une équipe n'avait plus de soldat mais le tour d'attaque n'a pas été skip
 
 # NOUVEAUTÉS :
-# TODO : Ajouter Region.coast -> endroits où on peut mettre les bateaux
-# TODO : Permettre d'attaquer avec un bateau les régions qu'on a en carte
 # TODO : permettre de paramétrer la quantité d'animations
+# TODO : quand les règles seront terminées, faire un tutoriel
 
 # RÈGLES :
 # TODO : Définir une utilité pour les cartes des régions occupées
 # TODO : Choisir si une région a un nombre max de bateaux du même peuple
 # TODO : Quand on achète une carte, on tire 3 cartes et on choisit celle qu'on veut
+# TODO : Après un tour, on peut revendre un bateau pour 1 pièce
+# TODO : Quand on transporte un bateau, on peut emmener le drapeau avec
 
 # IDÉES :
 # NOTE for v.1.5 : jouer en réseau
